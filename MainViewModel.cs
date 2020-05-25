f88378c6cadf417b6b6ea7f375b0d7fa7c4e7d35
ievgeniy93 <63505406+ievgeniy93@users.noreply.github.com>
2020-05-25 19:12:37 +0300

    Добавьте файлы проекта.

---
+++ MainViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ATMEmulator.Models;
+
+namespace ATMEmulator
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        public MainViewModel()
+        {
+            ATM = new ATM();
+            ATM.DisplayText = "Добро пожаловать!";
+
+            MakeMoneyCommand = new RelayCommand(x => MakeMoney());
+        }
+
+        public ATM ATM { get; set; }
+
+        public RelayCommand MakeMoneyCommand { get; set; }
+
+        public void MakeMoney()
+        {
+            ATM.DisplayText = "куку!";
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
---
+++ Models/ATM.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATMEmulator.Models
+{
+    public class ATM : INotifyPropertyChanged
+    {
+        public ATM()
+        {
+            Banknotes = new List<Banknote>();
+        }
+
+        private string _displayText;
+        /// <summary>
+        /// Текст выводимый на экран банкомата
+        /// </summary>
+        public string DisplayText
+        {
+            get
+            {
+                return _displayText;
+            }
+            set
+            { 
+                _displayText = value;
+                OnPropertyChanged("DisplayText");
+            }
+        }
+
+        /// <summary>
+        /// Сумма которую хочет снять пользователь
+        /// </summary>
+        public decimal Money { get; set; }
+
+        public List<Banknote> Banknotes { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged([CallerMemberName]string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+
+        public void GetMoney()
+        {
+            
+        }
+
+        public void MakeMoney()
+        {
+
+        }
+    }
+}
