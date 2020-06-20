e42c0af8a95f907a4b868bf632484ae23d416e28
ievgeniy93 <63505406+ievgeniy93@users.noreply.github.com>
2020-06-20 10:33:25 +0300

    Созданы свойства для карты по ТЗ , close #1

--- Models/card.cs
+++ Models/card.cs
@@ -6,7 +6,16 @@ using System.Threading.Tasks;
 
 namespace ATMEmulator.Models
 {
-    class Card
+    public class Card
     {
+        public string Individual { get; set; } //физическое лицо
+        public string BankName { get; set; }  //банк, который выпустил карту
+        public string CardType { get; set; } //тип карты (дебетовая или кредитная);
+        public int CreditLimit { get; set; } //кредитный лимит
+        public DateTime Validity { get; set; } //срок действия
+        public string Account { get; set; } //привязанный счёт
+        public bool NFC { get; set; } //поддержка бесконтактной оплаты
+        
     }
+
 }
