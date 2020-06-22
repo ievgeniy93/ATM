29e24626936a5026f6782cdcade60ac316577654
ievgeniy93 <63505406+ievgeniy93@users.noreply.github.com>
2020-06-22 10:10:47 +0300

    Исправлены замечания по классу Card, close #1

--- Models/card.cs
+++ Models/card.cs
@@ -5,17 +5,76 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace ATMEmulator.Models
-{
+{   
+    /// <summary>
+    /// Банковская карта. 
+    /// </summary>
     public class Card
     {
-        public string Individual { get; set; } //физическое лицо
-        public string BankName { get; set; }  //банк, который выпустил карту
-        public string CardType { get; set; } //тип карты (дебетовая или кредитная);
-        public int CreditLimit { get; set; } //кредитный лимит
-        public DateTime Validity { get; set; } //срок действия
-        public string Account { get; set; } //привязанный счёт
-        public bool NFC { get; set; } //поддержка бесконтактной оплаты
-        
+        /// <summary>
+        /// Физ. лицо.
+        /// </summary>
+        public Individual Individual { get; set; }
+
+        /// <summary>
+        /// Название банка.
+        /// </summary>
+        public BankName BankName { get; set; }
+
+        /// <summary>
+        /// Тип карты (дебетовая или кредитная).
+        /// </summary>
+        public CardType CardType { get; set; }
+
+        /// <summary>
+        /// Кредитный лимит.
+        /// </summary>
+        public int CreditLimit { get; set; }
+
+        /// <summary>
+        /// Срок действия.
+        /// </summary>
+        public DateTime Validity { get; set; }
+
+        /// <summary>
+        /// Привязанный счёт
+        /// </summary>
+        public string Account { get; set; }
+
+        /// <summary>
+        /// Поддержка бесконтактной оплаты
+        /// </summary>
+        public bool HasNFC { get; set; }
+         
     }
 
+
+    /// <summary>
+    /// Тип карты (дебитовая, кредитовая)
+    /// </summary>
+    public enum CardType
+    {
+        DebitCard,
+        CreditCard
+    }
+
+    /// <summary>
+    /// Свойства физ. лица (Имя, пол, возраст)
+    /// </summary>
+    public enum Individual
+    {
+        Name,
+        Sex,
+        Age
+    }
+
+    /// <summary>
+    /// Список банков
+    /// </summary>
+    public enum BankName
+    {
+        AlfaBank,
+        Sberbank,
+        HitBank
+    }
 }
